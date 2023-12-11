a2f64e9870281a955d14ac5e33814f2bd8c9878e
initprog <indrawan@nagwos.com>
2023-12-11 01:30:52 -0600

    minor

--- __main__.py
+++ __main__.py
@@ -1,7 +1,8 @@
 from qpos import app
 
 """
-Added this module to enable running application from VS Code
+Added this module to enable running application from VS Code.
+To run the application from terminal: py -m qpos
 """
 if __name__ == '__main__':
 	app.run()
