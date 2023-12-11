5303528980f54f080d07263ea3034f15a089da81
initprog <indrawan@nagwos.com>
2023-12-11 16:48:22 -0600

    add application icon

--- qpos/app.py
+++ qpos/app.py
@@ -1,8 +1,14 @@
 from PyQt6.QtWidgets import QApplication
+from PyQt6 import QtGui
 from qpos import user
-import sys
+import os, sys
 
 def run():
     app = QApplication(sys.argv)
     e = user.UserAuth()
     sys.exit(app.exec())
+
+def appicon():
+    """ Get application icon """
+    icon = QtGui.QIcon(os.path.join(os.path.dirname(__file__), f"asset{os.sep}Q.png"))
+    return icon
--- qpos/user.py
+++ qpos/user.py
@@ -1,49 +1,38 @@
-from PyQt6.QtWidgets import *
-from PyQt6 import QtWidgets
-from PyQt6 import QtGui, QtWidgets
-from PyQt6.QtWidgets import *
-from PyQt6.QtCore import *
-
+from PyQt6.QtWidgets import QWidget, QMessageBox, QMainWindow, QDialogButtonBox
+from PyQt6 import QtGui
+from qpos import app
 from qpos.ui import initial, orderMain
 
 class UserAuth(initial.Ui_Form):
     def __init__(self):
-        self.Form = QtWidgets.QWidget()
-        self.setupUi(self.Form)
-        self.Form.show()
+        self.win = QWidget()
+        self.setupUi(self.win)
+        self.win.setWindowIcon(app.appicon())
+        self.win.show()
         self.initUI()
 
     def initUI(self):
         # connect button to function
         self.isPassword.clicked.connect(self.checkPassword)
     def showMessageBox(self,title,message):
-        msgBox = QtWidgets.QMessageBox()
-        msgBox.setIcon(QtWidgets.QMessageBox.Icon.Warning)
+        msgBox = QMessageBox()
+        msgBox.setIcon(QMessageBox.Icon.Warning)
         msgBox.setWindowTitle(title)
         msgBox.setText(message)
-        msgBox.setStandardButtons(QtGui.QMessageBox.Icon.Ok)
-        msgBox.exec_()
+        msgBox.setStandardButtons(QMessageBox.StandardButton.Ok)
+        msgBox.exec()
+        
     def checkPassword(self):
         if self.passwordInput.text() == '1234':
             self.openOrder()
         elif self.passwordInput.text() == '':
-            self.showMessageBox('오류','비밀번호를 입력하십시오')
-            #warning = QMessageBox()
-            #warning.setIcon(QMessageBox.Warning)
-            #warning.setText("비밀번호를 입력하십시오")
-            #warning.setWindowTitle("오류")
-            #warning.exec()
+            self.showMessageBox('Error', 'Please enter a password')
         else:
-            self.showMessageBox('오류','비밀번호가 올바르지 않습니다')
-            #warning = QMessageBox()
-            #warning.setIcon(QMessageBox.Warning)
-            #warning.setText("비밀번호가 올바르지 않습니다")
-            #warning.setWindowTitle("오류")
-            #warning.exec()
+            self.showMessageBox('Error', 'Incorrect password.')
 
     # make new window of SaleStat
     def openOrder(self):
-        self.Form = QtWidgets.QMainWindow()
+        self.win = QMainWindow()
         self.ds = Order()
 
 class Order(QMainWindow):
