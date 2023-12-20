0e65ad56679c584b5851a55d5b966b5588f3f190
indrawan <indrawan@nagwos.com>
2023-12-20 17:10:52 -0600

    show app icon on win11 taskbar

--- qpos/app.py
+++ qpos/app.py
@@ -1,10 +1,17 @@
+import platform, ctypes
 from PyQt6.QtWidgets import QApplication
 from PyQt6 import QtGui
 from qpos import login, main, user
 import os, sys
 
+QPOS_APPID = 'nagwos.commerce.pos'
+
 def run():
     app = QApplication(sys.argv)
+    # Show app's icon on Windows 11 taskbar
+    if platform.system() == 'Windows' and platform.release() == '11':
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(QPOS_APPID)
+
     win = main.MainApp()
     win.show()
     
