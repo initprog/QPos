300fabb5d5524b0926629660197f09f579a4efe2
indrawan <indrawan@nagwos.com>
2023-12-28 18:16:48 -0600

    Add singleton for db connection

--- qpos/item.py
+++ qpos/item.py
@@ -1,23 +1,20 @@
 import os
 from PyQt6.QtWidgets import QWidget
 from PyQt6.QtSql import QSqlDatabase, QSqlTableModel, QSqlRelation, QSqlRelationalTableModel, QSqlRelationalDelegate
-from qpos.db import DEFAULT_DB
+from qpos.db import Qdb
 from qpos.view.item_ui import Ui_Form
-
-db = QSqlDatabase("QSQLITE")
-db.setDatabaseName(DEFAULT_DB)
-db.open()
 
 class Item(QWidget):
     def __init__(self):
         super(Item, self).__init__()
         self.ui = Ui_Form()
         self.ui.setupUi(self)
-        self.model = QSqlRelationalTableModel(db=db)
+        self.db = Qdb()
+        self.model = QSqlRelationalTableModel(db=self.db)
         self.model.setTable("Item")
         self.ui.tvItem.setModel(self.model)
 
-        self.model_variation = QSqlRelationalTableModel(db=db)
+        self.model_variation = QSqlRelationalTableModel(db=self.db)
         self.model_variation.setTable("ItemVariation")
         self.ui.tvVariation.setModel(self.model_variation)
 
